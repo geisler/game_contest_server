86c2266f76c2614f2774160d530fb15f32ad569d
Jonathan Geisler <jgeisler@cse.taylor.edu>
2013-09-11 09:17:51 -0400

    Initial commit
    Beginning code demonstrated in class.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameContestServer::Application.config.secret_key_base = 'c63dfc552dc7f4c80602fa195b872a1ad84723535384e7c14974ecba7a30ed3ad4a927e8e6b06da299222e2d449d44a5ffed9ab86894ae4034427287900df16e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GameContestServer::Application.config.session_store :cookie_store, key: '_game_contest_server_session'
