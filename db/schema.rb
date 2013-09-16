d5931f92f8310d807d4c93cea376042b649edf36
Jonathan Geisler <jgeisler@cse.taylor.edu>
2013-09-16 00:33:09 -0400

    Initial User model tests
    Some basic tests in the User model to check for fields that should be
    a part of any user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913024647) do
+ActiveRecord::Schema.define(version: 20130916043159) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to(:username) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
 end
