498103f23e6dc48665264c58d3f06a54fe37bbd8
Jonathan Geisler <jgeisler@cse.taylor.edu>
2013-09-24 23:34:56 -0400

    Pass email formatting tests.
    This commit adds a regular expression to the email validation
    directive to ensure most valid email addresses are accepted and not
    out invalid ones from getting through.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true, length: { maximum: 25 }
-  validates :email, presence: true
+  #
+  # Note that the email regex is from Michael Hartl's Rails tutorial
+  # and does a "good enough" job.  It misses some addresses that are
+  # officially acceptable, but silly to check for.  He even has a more
+  # robust email regex in his homework section at the end of Chapter 6.
+  #
+  validates :email, presence: true,
+		    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
