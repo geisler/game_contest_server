df0ada3b60b241a037fa7aa1c5d8d16f0ad68946
Jonathan Geisler <jgeisler@cse.taylor.edu>
2013-10-10 23:01:20 -0400

    Simple tests for the Bootstrap navigation bar
    This test simply checks that we've tried to put in a navigation bar
    and the corresponding links.  It needs some fixing that will make it
    more specific when a particular bug can be traced, but for now is
    "good enough."

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require 'capybara/rspec'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -39,6 +41,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-
-  config.include Capybara::DSL
 end
