63e7100a1f0c6b733f1f9070850a32ef374ca301
Jonathan Geisler <jgeisler@cse.taylor.edu>
2013-10-11 14:40:47 -0400

    Fix within issue.
    The previous testing commit had the within commented out because it
    wasn't inside an it block.  Yeah, Stack Overflow
    
        http://stackoverflow.com/questions/19309542/undefined-method-within-using-capybara-and-rspec
    
    for helping me debug this simple issue.

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -8,11 +8,12 @@ feature "HomePage" do
   describe "the navigation bar" do
     it { should have_selector('.navbar') }
 
-# get this working!!!
-#    within ".navbar" do
-      it { should have_link('Game Contest Server', href: root_path) }
-      it { should have_link('Users', href: users_path) }
-      it { should have_link('Sign Up', href: signup_path) }
-#    end
+    it "has the proper links" do
+      within ".navbar" do
+	should have_link('Game Contest Server', href: root_path)
+	should have_link('Users', href: users_path)
+	should have_link('Sign Up', href: signup_path)
+      end
+    end
   end
 end
