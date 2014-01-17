31aa36e9f7a94ec69380cf4ebc9940c7d9936faf
David Nicholls <david_nicholls@taylor.edu>
2014-01-17 18:32:24 +0000

    Merging with dev.

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20140116224848) do
     t.integer  "user_id"
     t.integer  "referee_id"
     t.text     "description"
-    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deadline"
@@ -26,7 +25,6 @@ ActiveRecord::Schema.define(version: 20140116224848) do
 
   add_index "contests", ["name"], name: "index_contests_on_name", unique: true
   add_index "contests", ["referee_id"], name: "index_contests_on_referee_id"
-  add_index "contests", ["slug"], name: "index_contests_on_slug", unique: true
   add_index "contests", ["user_id"], name: "index_contests_on_user_id"
 
   create_table "friendly_id_slugs", force: true do |t|
@@ -79,7 +77,6 @@ ActiveRecord::Schema.define(version: 20140116224848) do
     t.integer  "contest_id"
     t.string   "file_location"
     t.integer  "programming_language_id"
-    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
@@ -91,7 +88,6 @@ ActiveRecord::Schema.define(version: 20140116224848) do
   add_index "players", ["contest_id"], name: "index_players_on_contest_id"
   add_index "players", ["name"], name: "index_players_on_name", unique: true
   add_index "players", ["programming_language_id"], name: "index_players_on_programming_language_id"
-  add_index "players", ["slug"], name: "index_players_on_slug", unique: true
   add_index "players", ["user_id"], name: "index_players_on_user_id"
 
   create_table "programming_languages", force: true do |t|
@@ -103,7 +99,6 @@ ActiveRecord::Schema.define(version: 20140116224848) do
   create_table "referees", force: true do |t|
     t.string   "file_location"
     t.integer  "programming_language_id"
-    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
@@ -114,7 +109,6 @@ ActiveRecord::Schema.define(version: 20140116224848) do
 
   add_index "referees", ["name"], name: "index_referees_on_name", unique: true
   add_index "referees", ["programming_language_id"], name: "index_referees_on_programming_language_id"
-  add_index "referees", ["slug"], name: "index_referees_on_slug", unique: true
   add_index "referees", ["user_id"], name: "index_referees_on_user_id"
 
   create_table "tournaments", force: true do |t|
@@ -123,16 +117,12 @@ ActiveRecord::Schema.define(version: 20140116224848) do
     t.datetime "start"
     t.string   "name"
     t.string   "status"
-    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "tournaments", ["slug"], name: "index_tournaments_on_slug", unique: true
-
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
@@ -143,7 +133,6 @@ ActiveRecord::Schema.define(version: 20140116224848) do
     t.string   "chat_url"
   end
 
-  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
