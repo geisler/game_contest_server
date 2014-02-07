5521f77a5555e8cc36b13a6227e57c5a46c5b59b
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-02-07 15:20:30 -0500

    Change hard-coded path for new machine.
    The original hard-coded path was for the J-term VM.  This commit now
    points to my local directory.  A more permanent change that doesn't
    have a hard-coded path is needed, but I need to use the repository now
    before looking into how it can be fixed.

--- vendor/game_contest_server.sh
+++ vendor/game_contest_server.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 # Game contest sever match listener
 
-cd ../../home/jgeisler/game_contest_server
+cd ../../home/jgeisler/classes/cos243/game_contest_server.git
 clockworkd -d . start ./clock.rb --log
