ba6bad631d05cedbbc7125cef63d9333120a3d51
Becky Hogan <bhogan@game.(none)>
2015-01-09 14:09:00 -0500

    challenge feature and frontend fully functional

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,13 +2,13 @@ class MatchesController < ApplicationController
   before_action :ensure_user_logged_in, except: [:index, :show]
  
  def new
-    contest = Contest.friendly.find(params[:contest_id])
-    @contest = Contest.friendly.find(params[:contest_id])
-    @match = contest.matches.build
-    @match.manager.players.each do |f|
- 	@match.player_matches.build(player: f )
-    end
-    @match.earliest_start = Time.now
+   contest = Contest.friendly.find(params[:contest_id])
+   @contest = Contest.friendly.find(params[:contest_id])
+   @match = contest.matches.build
+   @match.manager.players.each do |f|
+	@match.player_matches.build(player: f )
+   end
+   @match.earliest_start = Time.now
   
   end 
 
@@ -16,7 +16,6 @@ class MatchesController < ApplicationController
   def create
     @contest = Contest.friendly.find(params[:contest_id])
     @match = @contest.matches.build(acceptable_params)
-    @match.player_ids << params[:match][:player_id] 
     @match.status = "waiting"
     if @match.save
 	flash[:success] = 'Match created.'
@@ -36,10 +35,14 @@ class MatchesController < ApplicationController
     @matches = @tournament.matches
   end
 
+
   private
 
   def acceptable_params
     params.require(:match).permit(:earliest_start, player_ids: @contest.players.try(:ids))
   end
+
+
+
 
 end
