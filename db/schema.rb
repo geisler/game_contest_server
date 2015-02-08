c1dc09727ee004e372a9f08bfa80a11462b08faa
Jonathan Geisler <jgeisler@cse.taylor.edu>
2015-02-08 02:36:17 -0500

    Have schema reflect updated database
    This ensures that the schema file is up-to-date with all the changes to the
    database compared with the migrations committed earlier.

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20150122225259) do
 
-  create_table "contests", force: true do |t|
+  create_table "contests", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "referee_id"
     t.text     "description"
-    t.string   "slug"
+    t.string   "slug",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deadline"
-    t.string   "name"
+    t.string   "name",        limit: 255
   end
 
   add_index "contests", ["name"], name: "index_contests_on_name", unique: true
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(version: 20150122225259) do
   add_index "contests", ["slug"], name: "index_contests_on_slug", unique: true
   add_index "contests", ["user_id"], name: "index_contests_on_user_id"
 
-  create_table "friendly_id_slugs", force: true do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",           limit: 255, null: false
+    t.integer  "sluggable_id",               null: false
     t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+    t.string   "scope",          limit: 255
     t.datetime "created_at"
   end
 
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 20150122225259) do
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
-  create_table "match_paths", force: true do |t|
+  create_table "match_paths", force: :cascade do |t|
     t.integer  "parent_match_id"
     t.integer  "child_match_id"
-    t.string   "result"
+    t.string   "result",          limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -53,14 +53,14 @@ ActiveRecord::Schema.define(version: 20150122225259) do
   add_index "match_paths", ["child_match_id"], name: "index_match_paths_on_child_match_id"
   add_index "match_paths", ["parent_match_id"], name: "index_match_paths_on_parent_match_id"
 
-  create_table "matches", force: true do |t|
+  create_table "matches", force: :cascade do |t|
     t.integer  "manager_id"
     t.datetime "completion"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "status"
+    t.string   "status",         limit: 255
     t.datetime "earliest_start"
-    t.string   "manager_type"
+    t.string   "manager_type",   limit: 255
     t.string   "slug"
     t.integer  "rounds"
   end
@@ -69,37 +69,37 @@ ActiveRecord::Schema.define(version: 20150122225259) do
   add_index "matches", ["manager_id"], name: "index_matches_on_manager_id"
   add_index "matches", ["slug"], name: "index_matches_on_slug", unique: true
 
-  create_table "player_matches", force: true do |t|
+  create_table "player_matches", force: :cascade do |t|
     t.integer  "player_id"
     t.integer  "match_id"
     t.float    "score"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "result"
+    t.string   "result",     limit: 255
   end
 
   add_index "player_matches", ["match_id"], name: "index_player_matches_on_match_id"
   add_index "player_matches", ["player_id"], name: "index_player_matches_on_player_id"
 
-  create_table "player_tournaments", force: true do |t|
+  create_table "player_tournaments", force: :cascade do |t|
     t.integer  "tournament_id"
     t.integer  "player_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "players", force: true do |t|
+  create_table "players", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "contest_id"
-    t.string   "file_location"
+    t.string   "file_location",           limit: 255
     t.integer  "programming_language_id"
-    t.string   "slug"
+    t.string   "slug",                    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
-    t.string   "name"
-    t.boolean  "downloadable",            default: false
-    t.boolean  "playable",                default: true
+    t.string   "name",                    limit: 255
+    t.boolean  "downloadable",                        default: false
+    t.boolean  "playable",                            default: true
   end
 
   add_index "players", ["contest_id"], name: "index_players_on_contest_id"
@@ -108,26 +108,26 @@ ActiveRecord::Schema.define(version: 20150122225259) do
   add_index "players", ["slug"], name: "index_players_on_slug", unique: true
   add_index "players", ["user_id"], name: "index_players_on_user_id"
 
-  create_table "programming_languages", force: true do |t|
-    t.string   "name"
+  create_table "programming_languages", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "referees", force: true do |t|
-    t.string   "file_location"
+  create_table "referees", force: :cascade do |t|
+    t.string   "file_location",            limit: 255
     t.integer  "programming_language_id"
-    t.string   "slug"
+    t.string   "slug",                     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "rules_url"
+    t.string   "name",                     limit: 255
+    t.string   "rules_url",                limit: 255
     t.integer  "players_per_game"
     t.integer  "user_id"
-    t.integer  "match_limit"
-    t.boolean  "rounds_capable"
     t.string   "compressed_file_location"
+    t.integer  "match_limit"
     t.integer  "time_per_game"
+    t.boolean  "rounds_capable"
   end
 
   add_index "referees", ["name"], name: "index_referees_on_name", unique: true
@@ -135,13 +135,13 @@ ActiveRecord::Schema.define(version: 20150122225259) do
   add_index "referees", ["slug"], name: "index_referees_on_slug", unique: true
   add_index "referees", ["user_id"], name: "index_referees_on_user_id"
 
-  create_table "tournaments", force: true do |t|
-    t.string   "tournament_type"
+  create_table "tournaments", force: :cascade do |t|
+    t.string   "tournament_type",  limit: 255
     t.integer  "contest_id"
     t.datetime "start"
-    t.string   "name"
-    t.string   "status"
-    t.string   "slug"
+    t.string   "name",             limit: 255
+    t.string   "status",           limit: 255
+    t.string   "slug",             limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "rounds_per_match"
@@ -149,17 +149,17 @@ ActiveRecord::Schema.define(version: 20150122225259) do
 
   add_index "tournaments", ["slug"], name: "index_tournaments_on_slug", unique: true
 
-  create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "slug"
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        limit: 255
+    t.string   "slug",            limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email"
-    t.string   "password_digest"
-    t.boolean  "admin",           default: false
-    t.boolean  "contest_creator", default: false
-    t.boolean  "banned",          default: false
-    t.string   "chat_url"
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.boolean  "admin",                       default: false
+    t.boolean  "contest_creator",             default: false
+    t.boolean  "banned",                      default: false
+    t.string   "chat_url",        limit: 255
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true
